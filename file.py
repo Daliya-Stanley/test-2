1b7daea3eb9edee64183490563ceaab21af37b78
Daliya Stanley <daliya.kurien@gmail.com>
2025-02-04 21:00:38 +0000

    TRIED adding comment

--- file.py
+++ file.py
@@ -4,7 +4,7 @@
 #     line = input("Enter a message to echo or type 'done' to complete: ")
 #     print('<', line, '>')
 # learnt about debugging button
-
+from tkinter.font import names
 
 numbers = [23, 67, 32, 9, 77]
 
@@ -15,3 +15,10 @@ while numbers:
     print(numbers.pop() * 2)
 print("Empty list")
 print("Carry on from here onnn...")
+
+print(   r'r\n \1\2\3')
+print("hello\aworld")
+
+name = "Fred"\"Bloggs"
+#lets see what happens at the github
+
